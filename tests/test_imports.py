3416fbc136f9a05b531336645dfacb8663e983d3
fobiasmog <zbrejkin@yandex.ru>
2026-03-09 14:08:14 +0100

    slim_bindings 1.2.0

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -6,9 +6,11 @@ import pytest
 
 def test_slim_bindings_import_fails():
     with pytest.raises(ImportError):
-        from slim_bindings.slim_bindings import SessionConfig, SessionType
+        from slim_bindings.slim_bindings import SessionConfig, SessionType  # noqa: F401
+
 
 def test_slim_bindings_import_succeeds():
     from slim_bindings import SessionConfig, SessionType
+
     assert SessionConfig is not None
     assert SessionType is not None
